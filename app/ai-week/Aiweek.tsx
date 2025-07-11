48cce8f52ed15ef1b86066467a5bcf46c19ef0ec
Md Israkul Islam <israkulislam@gmail.com>
2025-07-11 11:47:25 +0600

    added

--- app/ai-week/Aiweek.tsx
+++ app/ai-week/Aiweek.tsx
@@ -2,13 +2,20 @@ import React from 'react'
 
 export default function Aiweek() {
   return (
-    <div>
-        <h1 className='h-[24px] w-[600px] font-semibold text-4xl text-white text-center justify-center'>
-            AI Start Up Of The Week
-        </h1>
-        <p className=''>Tools with the highest recent growth this week</p>
-        <button className='btn text-white'>Discover Now</button>
-    </div>
+    <div className="bg-black min-h-screen text-white flex flex-col items-center justify-center py-8 sm:py-12 lg:py-16 px-4 sm:px-6 lg:px-8">
+  <div className="text-center space-y-6">
+    <h1 className="font-semibold text-3xl sm:text-4xl lg:text-5xl max-w-2xl mx-auto">
+      AI Start Up Of The Week
+    </h1>
+    <p className="text-base sm:text-lg text-white">
+      Tools with the highest recent growth this week
+    </p>
+    <button className="border rounded-md px-6 py-3 text-white hover:bg-white bg-red-500 hover:text-black transition duration-300">
+      Discover Now
+    </button>
+  </div>
+</div>
+
   )
 }
 //all
---
+++ app/ai-week/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Aiweek from './Aiweek'
+
+export default function page() {
+  return (
+    <div>
+        <Aiweek></Aiweek>
+    </div>
+  )
+}
